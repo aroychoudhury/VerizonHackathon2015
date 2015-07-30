7fe9cb70b286fe13d1efc1ceca9bb43480343355
Abhishek Roychoudhury <abhishek.roychoudhury@live.com>
2015-07-30 03:35:15 -0700

    DB Diagrams

---
+++ diagram/CodeAvengers_create.sql
@@ -0,0 +1,118 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2015-07-29 12:04:56.058
+
+
+
+
+-- sequences
+-- Sequence: categoryId
+-- 
+
+
+CREATE SEQUENCE categoryId
+      INCREMENT BY 1
+      NO MINVALUE
+      NO MAXVALUE
+      START WITH 1
+      NO CYCLE
+;
+
+
+-- Sequence: locationId
+-- 
+
+
+CREATE SEQUENCE locationId
+      INCREMENT BY 1
+      NO MINVALUE
+      NO MAXVALUE
+      START WITH 1
+      NO CYCLE
+;
+
+
+
+
+
+-- tables
+-- Table: LOCATION_DETAILS
+CREATE TABLE LOCATION_DETAILS (
+    latitude varchar(20)  NOT NULL,
+    longitude varchar(20)  NOT NULL,
+    locationId int  NOT NULL,
+   CONSTRAINT LOCATION_DETAILS_pk PRIMARY KEY (locationId)
+);
+
+-- Table: LOCATION_MASTER
+CREATE TABLE LOCATION_MASTER (
+    locationId int  NOT NULL,
+    area varchar(50)  NOT NULL,
+    code varchar(10)  NOT NULL,
+    parentLocationId int  NOT NULL,
+   CONSTRAINT LOCATION_MASTER_pk PRIMARY KEY (locationId)
+);
+
+CREATE INDEX LocationMaster_idx_1 on LOCATION_MASTER (area ASC);
+
+
+
+-- Table: REPORT_ASSOCIATION
+CREATE TABLE REPORT_ASSOCIATION (
+    categoryField varchar(20)  NOT NULL,
+    categoryValue varchar(20)  NOT NULL,
+    reportId int  NOT NULL,
+    categoryId int  NOT NULL,
+    locationId int  NOT NULL,
+   CONSTRAINT REPORT_ASSOCIATION_pk PRIMARY KEY (reportId)
+);
+
+-- Table: REPORT_CATEGORY
+CREATE TABLE REPORT_CATEGORY (
+    reportCategory varchar(20)  NOT NULL,
+    categoryId int  NOT NULL,
+   CONSTRAINT REPORT_CATEGORY_pk PRIMARY KEY (categoryId)
+);
+
+
+
+
+
+-- foreign keys
+-- Reference:  LOCATION_DETAILS_LOCATION_MASTER (table: LOCATION_DETAILS)
+
+
+ALTER TABLE LOCATION_DETAILS ADD CONSTRAINT LOCATION_DETAILS_LOCATION_MASTER 
+    FOREIGN KEY (locationId)
+    REFERENCES LOCATION_MASTER (locationId)
+;
+
+-- Reference:  LOCATION_MASTER_LOCATION_MASTER (table: LOCATION_MASTER)
+
+
+ALTER TABLE LOCATION_MASTER ADD CONSTRAINT LOCATION_MASTER_LOCATION_MASTER 
+    FOREIGN KEY (parentLocationId)
+    REFERENCES LOCATION_MASTER (locationId)
+;
+
+-- Reference:  REPORT_ASSOCIATION_LOCATION_MASTER (table: REPORT_ASSOCIATION)
+
+
+ALTER TABLE REPORT_ASSOCIATION ADD CONSTRAINT REPORT_ASSOCIATION_LOCATION_MASTER 
+    FOREIGN KEY (locationId)
+    REFERENCES LOCATION_MASTER (locationId)
+;
+
+-- Reference:  REPORT_ASSOCIATION_REPORT_CATEGORY (table: REPORT_ASSOCIATION)
+
+
+ALTER TABLE REPORT_ASSOCIATION ADD CONSTRAINT REPORT_ASSOCIATION_REPORT_CATEGORY 
+    FOREIGN KEY (categoryId)
+    REFERENCES REPORT_CATEGORY (categoryId)
+;
+
+
+
+
+
+-- End of file.
+
