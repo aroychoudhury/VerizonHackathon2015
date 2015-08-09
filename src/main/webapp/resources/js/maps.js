8e573f6ed55aa82c161c18ce7d532809f4c16ade
Abhishek Roychoudhury <abhishek.roychoudhury@live.com>
2015-08-10 02:43:40 +0530

    Adding actual data display

--- src/main/webapp/resources/js/maps.js
+++ src/main/webapp/resources/js/maps.js
@@ -1,3 +1,6 @@
+var MAP_MINIMUM_ZOOM = 4;
+var MAP_MAXIMUM_ZOOM = 6;
+
 var MAP_INSTANCE = null;
 var MAP_STYLES = [{
     featureType: 'landscape.man_made',
@@ -91,6 +94,8 @@ var MAP_STYLES = [{
     }]
 }];
 
+var LOCATIONS_CACHE = {};
+
 var getMap = function() {
     if (null == MAP_INSTANCE) {
         MAP_INSTANCE = new GMaps({
@@ -123,22 +128,25 @@ var populateMap = function(list) {
             var item = list[i];
             MAP_INSTANCE.addMarker({
                 lat: item['lat'],
-                lng: item['lng'],
+                lng: item['lon'],
                 icon: 'resources/img/map/red.png',
+				infoWindow: {
+				    content: item['content']
+				},
                 click: function(marker) {
                     var markersArray = MAP_INSTANCE.markers;
                     for (var i = 0; i < markersArray.length; i++) {
                         markersArray[i].setIcon('resources/img/map/red.png');
                     }
-                    MAP_INSTANCE.setZoom(8);
+                    MAP_INSTANCE.setZoom(MAP_MAXIMUM_ZOOM);
                     marker.setIcon('resources/img/map/green.png');
                     MAP_INSTANCE.map.panTo(marker.position);
-                    $('#largeModal').modal();
+                    //$('#largeModal').modal();
                     refreshMap();
                 }
             });
         }
-        MAP_INSTANCE.setZoom(4);
+        MAP_INSTANCE.setZoom(MAP_MINIMUM_ZOOM);
         refreshMap();
     }
     return MAP_INSTANCE;
@@ -146,23 +154,41 @@ var populateMap = function(list) {
 
 var resetZoomOnMap = function(list) {
     if (null != MAP_INSTANCE) {
-        MAP_INSTANCE.setZoom(4);
+        MAP_INSTANCE.setZoom(MAP_MINIMUM_ZOOM);
         refreshMap();
     }
     return MAP_INSTANCE;
 };
 
-var mockData = function() {
-    var list = [];
-    list.push({
-        lat: 20.385181,
-        lng: 72.911453
+var loadLocations = function(selectedId, refreshData) {
+    if (!refreshData && LOCATIONS_CACHE[selectedId] && 0 != LOCATIONS_CACHE[selectedId].length) {
+        populateMap(LOCATIONS_CACHE[selectedId]);
+        return;
+    }
+
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'categories/' + selectedId.substring(3),
+        success: function(responseData) {
+            var locationsFinal = [];
+            var locations = responseData.data.locns;
+            for (var idx = 0; idx < locations.length; idx++) {
+                var location = locations[idx];
+                locationsFinal.push({
+                    lat: parseFloat(location.lat),
+                    lon: parseFloat(location.lon),
+                    content: '<p style="font-weight: bold; line-height: 1; margin: 5px 0;">' 
+                    	+ location.area + '</p><p style="line-height: 1; margin: 5px 0;">' 
+                    	+ location.value + ' ' + location.unit + '</p>'
+                });
+            }
+            LOCATIONS_CACHE[selectedId] = locationsFinal;
+            populateMap(locationsFinal);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+        }
     });
-    list.push({
-        lat: 18.296974,
-        lng: 83.896782
-    });
-    return list;
 }
 
 $(document).ready(function() {
@@ -182,13 +208,12 @@ $(document).ready(function() {
                 $('#categoryTxt').text($(this).text());
                 $('#categoryTxt').val($(this).text());
                 $('#categoryTxt').attr('selectedId', $(this).attr('id'));
-                populateMap(mockData());
+                loadLocations($(this).attr('id'), true);
             });
             $('#map').show();
             getMap();
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            //TODO
         }
     });
 
